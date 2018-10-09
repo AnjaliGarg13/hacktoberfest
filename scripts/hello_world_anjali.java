d717f604857a45172a1db09a129100bbe97d1716
AnjaliGarg13 <38766449+AnjaliGarg13@users.noreply.github.com>
2018-10-09 23:10:08 +0530

    Create hello_world_anjali.java

---
+++ scripts/hello_world_anjali.java
@@ -0,0 +1,11 @@
+//Language : Java
+// Author : AnjaliGarg13
+//GitHUb Profile: https://github.com/AnjaliGarg13
+
+
+public class GitHub_Hacktoberfest{
+	
+		public static void main(String[] args){
+			System.out.println("Hello, World!");
+		}
+}
